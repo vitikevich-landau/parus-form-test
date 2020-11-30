331682131bd7907673425df03b66f80bc6c3a684
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2020-11-30 15:09:56 +0700

    # add Структура для создания событыя в БД

---
+++ controllers/events.js
@@ -0,0 +1,9 @@
+const {event} = require(`../models/Event`);
+
+const create = async (req, res, next) => {
+
+};
+
+module.exports = {
+    create
+}
---
+++ models/Event.js
@@ -0,0 +1,8 @@
+class Event {
+
+}
+
+module.exports = {
+    event: new Event(),
+    Event
+}
