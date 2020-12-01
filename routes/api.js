47598003c4fa017a0d1ca3fd051913bf82e5211f
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2020-12-01 22:08:29 +0700

    # refactoring

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 const express = require(`express`);
 const {verifyByPhone} = require(`../controllers/clients`);
-const {create} = require(`../controllers/events`);
+const {register} = require(`../controllers/questions`);
 const router = express.Router();
 
 router.get(`/verify-phone`, verifyByPhone);
-router.post(`/send`, create);
+router.post(`/send`, register);
 
 module.exports = router;
