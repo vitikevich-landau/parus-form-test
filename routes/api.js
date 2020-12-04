059c69cd8a7a21d07c51a6f128c4c22d07242032
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2020-12-04 10:34:20 +0700

    # Валидаторы вынесены

--- routes/api.js
+++ routes/api.js
@@ -2,16 +2,9 @@ const express = require(`express`);
 const {verifyByPhone} = require(`../controllers/clients`);
 const {register} = require(`../controllers/questions`);
 
-const {check} = require('express-validator');
-
 const router = express.Router();
 
-router.get(
-    `/verify-phone`,
-    [check(`phone`).matches(/\+7 \(([0-9]{3})\) ([0-9]{3}) ([0-9]{4})/)],
-    verifyByPhone
-);
-
+router.get(`/verify-phone`, verifyByPhone);
 router.post(`/send`, register);
 
 module.exports = router;
