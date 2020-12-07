775b57fc0eb7a0e0a524e6b658ba8d7467a201c3
Viktor <60886359+vitikevich-landau@users.noreply.github.com>
2020-12-07 15:49:27 +0700

    # add recaptcha, без серверной части

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,14 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
-    res.render('index', {title: 'Задать вопрос'});
+    const {host} = req.headers;
+
+    if (host.includes(`176.50.223.119`)) {
+        res.render('index-recaptcha', {title: 'Задать вопрос'});
+    } else {
+        res.render('index', {title: 'Задать вопрос'});
+    }
+
 });
 
 
